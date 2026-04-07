585a5ff019f8bd541718addb0a1771913cc71ade
iDevMirza <dev.mirzamahmud@gmail.com>
2026-04-07 15:00:17 +0100

    fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:personal_expense_tracker/main.dart';
+import 'package:personal_expense_tracker/personal_expense_tracker.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PersonalExpenseTracker());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
