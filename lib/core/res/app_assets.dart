dfa1534d5907cee98333c836dd73e26da1871cbe
iDevMirza <dev.mirzamahmud@gmail.com>
2026-04-07 14:59:30 +0100

    create app assets class to store all assets

---
+++ lib/core/res/app_assets.dart
@@ -0,0 +1,14 @@
+class AppAssets {
+  // App Images
+  static const String appLogo = 'assets/images/app_logo.png';
+
+  // App Icons
+  static const String homeInActive = 'assets/icons/home_inactive.svg';
+  static const String homeActive = 'assets/icons/home_active.svg';
+  static const String addInActive = 'assets/icons/add_inactive.svg';
+  static const String addActive = 'assets/icons/add_active.svg';
+  static const String historyInActive = 'assets/icons/history_inactive.svg';
+  static const String historyActive = 'assets/icons/history_active.svg';
+  static const String insightsInActive = 'assets/icons/insights_inactive.svg';
+  static const String insightsActive = 'assets/icons/insights_active.svg';
+}
