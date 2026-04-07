aa803718cff29caf360946a1613a25df079837a0
iDevMirza <dev.mirzamahmud@gmail.com>
2026-04-07 14:59:48 +0100

    create app string class to store all texts

---
+++ lib/core/res/app_strings.dart
@@ -0,0 +1,20 @@
+class AppStrings {
+  static const String appName = 'Personal Expense Tracker';
+  static const String appSubtitle = 'Track your spending, master your finances';
+
+  static const String nameFieldLabel = 'Name';
+  static const String nameFieldHint = 'Enter your full name';
+  static const String emailFieldLabel = 'Email';
+  static const String emailFieldHint = 'Enter your email';
+  static const String passwordFieldLabel = 'Password';
+  static const String passwordFieldHint = 'Enter your password';
+  static const String confirmPasswordFieldLabel = 'Confirm Password';
+  static const String confirmPasswordFieldHint = 'Re-type your password';
+
+  static const String rememberMeCheckText = 'Remember me';
+  static const String signInButtonText = 'Sign In';
+  static const String signUpButtonText = 'Sign Up';
+
+  static const String doNotHaveAccount = 'Don\'t have an account?';
+  static const String alreadyAccount = 'Already have an account?';
+}
