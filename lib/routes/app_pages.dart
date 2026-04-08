c2a0ad52b83ab747a80e5774166e84f24b402573
iDevMirza <dev.mirzamahmud@gmail.com>
2026-04-08 02:17:56 +0100

    add bottom nav pages

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -4,6 +4,10 @@ import 'package:personal_expense_tracker/views/authentication/sign_in/binding/si
 import 'package:personal_expense_tracker/views/authentication/sign_in/sign_in_activity.dart';
 import 'package:personal_expense_tracker/views/authentication/sign_up/binding/sign_up_binding.dart';
 import 'package:personal_expense_tracker/views/authentication/sign_up/sign_up_activity.dart';
+import 'package:personal_expense_tracker/views/bottom_navigation/add/add_activity.dart';
+import 'package:personal_expense_tracker/views/bottom_navigation/history/history_activity.dart';
+import 'package:personal_expense_tracker/views/bottom_navigation/home/home_activity.dart';
+import 'package:personal_expense_tracker/views/bottom_navigation/insights/insights_activity.dart';
 import 'package:personal_expense_tracker/views/intro/splash/binding/splash_binding.dart';
 import 'package:personal_expense_tracker/views/intro/splash/splash_activity.dart';
 
@@ -26,6 +30,30 @@ class AppPages {
         name: AppRoutes.signUpActivity,
         page: () => SignUpActivity(),
         binding: SignUpBinding()
+    ),
+
+    GetPage(
+        name: AppRoutes.homeActivity,
+        page: () => HomeActivity(),
+        // binding: SignUpBinding()
+    ),
+
+    GetPage(
+        name: AppRoutes.addActivity,
+        page: () => AddActivity(),
+        // binding: SignUpBinding()
+    ),
+
+    GetPage(
+        name: AppRoutes.historyActivity,
+        page: () => HistoryActivity(),
+        // binding: SignUpBinding()
+    ),
+
+    GetPage(
+        name: AppRoutes.insightsActivity,
+        page: () => InsightsActivity(),
+        // binding: SignUpBinding()
     )
   ];
 }
