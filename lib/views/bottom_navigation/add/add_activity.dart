4ac71e4d5e28c7ac56e588f11df3801672cc540c
iDevMirza <dev.mirzamahmud@gmail.com>
2026-04-09 00:19:35 +0100

    complete design of the add expense activity

--- lib/views/bottom_navigation/add/add_activity.dart
+++ lib/views/bottom_navigation/add/add_activity.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:personal_expense_tracker/core/res/app_colors.dart';
 import 'package:personal_expense_tracker/core/widgets/bottom_nav/custom_bottom_nav_bar.dart';
+import 'package:personal_expense_tracker/routes/app_routes.dart';
 
 class AddActivity extends StatelessWidget {
   const AddActivity({super.key});
@@ -10,8 +13,203 @@ class AddActivity extends StatelessWidget {
       top: false,
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Center(
-          child: Text('Add'),
+        appBar: AppBar(
+          backgroundColor: AppColors.primaryColor,
+          title: Text(
+            'Add Expense',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.white,
+              fontWeight: FontWeight.w600
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsetsDirectional.symmetric(vertical: 20, horizontal: 16),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: .start,
+              children: [
+                TextFormField(
+                  // controller: controller.emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                      labelText: 'Title',
+                      hintText: 'e.g. Groceries',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black.withValues(alpha: 0.4))
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black.withValues(alpha: 0.4))
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: AppColors.primaryColor)
+                      )
+                  ),
+                  validator: (value){
+
+                  },
+                ),
+                const SizedBox(height: 12),
+
+                TextFormField(
+                  // controller: controller.emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                      labelText: 'Amount (£)',
+                      hintText: '0.00',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black.withValues(alpha: 0.4))
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black.withValues(alpha: 0.4))
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: AppColors.primaryColor)
+                      )
+                  ),
+                  validator: (value){
+
+                  },
+                ),
+                const SizedBox(height: 12),
+
+                TextFormField(
+                  // controller: controller.emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                      labelText: 'Category',
+                      hintText: 'Select category',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black.withValues(alpha: 0.4))
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black.withValues(alpha: 0.4))
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: AppColors.primaryColor)
+                      )
+                  ),
+                  validator: (value){
+
+                  },
+                ),
+                const SizedBox(height: 12),
+
+                TextFormField(
+                  // controller: controller.emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                      labelText: 'Date',
+                      hintText: 'Select date',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black.withValues(alpha: 0.4))
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black.withValues(alpha: 0.4))
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: AppColors.primaryColor)
+                      )
+                  ),
+                  validator: (value){
+
+                  },
+                ),
+                const SizedBox(height: 12),
+
+                TextFormField(
+                  // controller: controller.emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                      labelText: 'Location',
+                      hintText: 'Select Location',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black.withValues(alpha: 0.4))
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black.withValues(alpha: 0.4))
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: AppColors.primaryColor)
+                      )
+                  ),
+                  validator: (value){
+
+                  },
+                ),
+                const SizedBox(height: 12),
+
+                TextFormField(
+                  // controller: controller.emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                      labelText: 'Note (Optional)',
+                      hintText: 'Enter some notes',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black.withValues(alpha: 0.4))
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black.withValues(alpha: 0.4))
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: AppColors.primaryColor)
+                      )
+                  ),
+                  validator: (value){
+
+                  },
+                ),
+                const SizedBox(height: 24),
+
+                ElevatedButton(
+                    onPressed: (){
+                      Get.offAllNamed(AppRoutes.homeActivity);
+                    },
+                    style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(8)),
+                        backgroundColor: AppColors.primaryColor,
+                        fixedSize: Size(MediaQuery.of(context).size.width, 48)
+                    ),
+                    child: Text(
+                      'Save Expense',
+                      textAlign: .center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14
+                      ),
+                    )
+                )
+              ],
+            ),
+          ),
         ),
         bottomNavigationBar: CustomBottomNavBar(selectedMenu: 1),
       ),
