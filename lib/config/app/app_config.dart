c71734f43046ca4c473b4900c4d27bfa28bb05bf
iDevMirza <dev.mirzamahmud@gmail.com>
2026-04-10 20:08:45 +0100

    add local storage instance

--- lib/config/app/app_config.dart
+++ lib/config/app/app_config.dart
@@ -1,4 +1,5 @@
 import 'package:personal_expense_tracker/services/firebase_service.dart';
+import 'package:personal_expense_tracker/services/local_storage_service.dart';
 
 class AppConfig {
   AppConfig._();
@@ -6,9 +7,13 @@ class AppConfig {
   static final AppConfig instance = AppConfig._();
 
   late FirebaseService _firebaseService;
+  late LocalStorageService _localStorageService;
 
   Future<void> initialize() async{
     _firebaseService = FirebaseService();
     await _firebaseService.initializeFirebase();
+
+    _localStorageService = LocalStorageService.instance;
+    await _localStorageService.database;
   }
 }
