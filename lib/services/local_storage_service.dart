326dc30b703f6d89e8b5b76ee55602ff8bf5738f
iDevMirza <dev.mirzamahmud@gmail.com>
2026-04-10 17:14:26 +0100

    create service for local storage

--- lib/services/local_storage_service.dart
+++ lib/services/local_storage_service.dart
@@ -1,9 +1,57 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
 class LocalStorageService {
   LocalStorageService._internal();
+  static final LocalStorageService instance = LocalStorageService._internal();
 
-  static final LocalStorageService _instance = LocalStorageService._internal();
+  Database? _database;
 
-  factory LocalStorageService(){
-    return _instance;
+  Future<Database> get database async {
+    _database ??= await _initDb('personal_expense_tracker_app.db');
+    return _database!;
+  }
+
+  Future<Database> _initDb(String fileName) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, fileName);
+
+    return openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE users (
+        id TEXT PRIMARY KEY,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE session (
+        id INTEGER PRIMARY KEY CHECK (id = 1),
+        user_id TEXT NOT NULL,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE expenses (
+        id TEXT PRIMARY KEY,
+        user_id TEXT NOT NULL,
+        amount REAL NOT NULL,
+        date TEXT NOT NULL,
+        title TEXT,
+        category TEXT,
+        notes TEXT
+      )
+    ''');
   }
 }
