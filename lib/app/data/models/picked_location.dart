4b4f0ca9e4c27d0cc9d6fd743389368166451777
iDevMirza <dev.mirzamahmud@gmail.com>
2026-05-14 01:05:53 +0100

    create model class for picking location

--- lib/app/data/models/picked_location.dart
+++ lib/app/data/models/picked_location.dart
@@ -1,11 +1,39 @@
+// class PickedLocation {
+//   final double latitude;
+//   final double longitude;
+//   final String address;
+//
+//   PickedLocation({
+//     required this.latitude,
+//     required this.longitude,
+//     required this.address,
+//   });
+// }
+
 class PickedLocation {
   final double latitude;
   final double longitude;
   final String address;
 
-  PickedLocation({
+  const PickedLocation({
     required this.latitude,
     required this.longitude,
     required this.address,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'latitude': latitude,
+      'longitude': longitude,
+      'address': address,
+    };
+  }
+
+  factory PickedLocation.fromJson(Map<String, dynamic> json) {
+    return PickedLocation(
+      latitude: (json['latitude'] as num).toDouble(),
+      longitude: (json['longitude'] as num).toDouble(),
+      address: json['address'] as String? ?? '',
+    );
+  }
 }
